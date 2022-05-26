ec74b8695c115e93933c01267769aee78f2fb6b6
Carlos Roberto Vilela Júnior <CIANDT\carlosjr@lnb035455cps.ciandt.global>
2022-05-26 11:51:53 -0300

    add dashboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:bytebank/screens/transferencias/lista.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BytebankApp());
+  runApp(const BytebankApp());
 }
 
 class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        primaryColor: Colors.green[900],
         colorScheme: ColorScheme.fromSwatch().copyWith(
             primary: Colors.green[800], secondary: Colors.blueAccent[700]),
         buttonTheme: ButtonThemeData(
@@ -17,7 +20,54 @@ class BytebankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: ListaDeTransferencias(),
+      home: Dashboard(),
     );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Theme.of(context).primaryColor,
+                height: 100,
+                width: 150,
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 32.0,
+                    ),
+                    Text(
+                      'Contatos',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
